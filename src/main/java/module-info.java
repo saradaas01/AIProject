c3365573e529491c8913a85e77dcd81905fdc6d4
saradaas01 <daassara01@gmail.com>
2025-12-13 00:20:09 +0200

    first pull

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -10,6 +10,7 @@ module com.example.tictactoe {
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
     requires com.almasb.fxgl.all;
+    requires javafx.graphics;
 
     opens com.example.tictactoe to javafx.fxml;
     exports com.example.tictactoe;
